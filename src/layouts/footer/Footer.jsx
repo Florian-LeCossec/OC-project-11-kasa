ce6907fa98848dce92d02eb7691dc489e3df6ea8
Florian-LeCossec <florian.le-cossec@orange.fr>
2024-12-12 10:45:18 +0100

    Handle header + add layout footer + add css

---
+++ src/layouts/footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <h1>Ceci est un footer</h1>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/layouts/header/Header.jsx
@@ -0,0 +1,30 @@
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import '@/styles/layouts/_header.scss';
+
+const Header = () => {
+  const location = useLocation();
+  
+  const getNavLinkClass = (path) => {
+    const baseClass = 'header__nav__item__link';
+    return `${baseClass} ${location.pathname === path ? `${baseClass}--active` : ''}`;
+  };
+
+  return (
+    <header className='header'>
+      <img className='header__logo' src='/src/assets/kasa-logo.svg' alt="logo" />
+      <nav className='header__nav'>
+        <ul className='header__nav__list'>
+          <li className='header__nav__item'>
+            <Link className={getNavLinkClass('/')} to="/">Accueil</Link>
+          </li>
+          <li className='header__nav__item'>
+            <Link className={getNavLinkClass('/about')} to="/about">A Propos</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
