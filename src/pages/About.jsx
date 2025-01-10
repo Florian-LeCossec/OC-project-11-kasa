a5161490663179ec8447c70f1b4be36b7740a681
Florian-LeCossec <florian.le-cossec@orange.fr>
2025-01-10 14:23:09 +0100

    handle about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,21 +1,27 @@
 import BtnList from '@/components/BtnList';
 import { useContext } from 'react';
 import DataContext from '@/contexts/dataContext';
+import Headband from '@/components/Headband';
+import '@/styles/pages/About.scss';
 
 const About = () => {
   const { dataAbout } = useContext(DataContext);
   const about = Array.isArray(dataAbout) ? dataAbout : [];
+  console.log(about);
   return (
-  <>
+  <div className='about'>
+    <Headband background={'/src/assets/landscape-2.svg'} />
+    <div className='about__btns-list'>
     {about.map((item) => (
-      <BtnList 
+      <BtnList
         key={item.aboutTitle}
         name={item.aboutTitle} 
         list={item.aboutText} 
         isList={false}
       />
     ))}
-  </>
+    </div>
+  </div>
   );
 };
 
