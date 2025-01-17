703cec411df2c40aa0459a6315f3f723ea4bd272
Florian-LeCossec <florian.le-cossec@orange.fr>
2025-01-17 09:07:24 +0100

    add responsive for header and homepage

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+import '@/styles/pages/NotFound.scss';
+
 const NotFound = () => {
     return (
-        <div>
-            <h1>404</h1>
+        <div className='not-found'>
+            <h1 className='not-found__title'>404</h1>
+            <h2 className='not-found__subtitle'>Oups! La page que vous cherchez n&apos;existe pas.</h2>
+            <Link className='not-found__link' to='/'>Retourner à la page d&apos;accueil</Link>
         </div>
     );
 };
