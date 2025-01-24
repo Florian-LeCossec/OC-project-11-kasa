c3875c613cd21a4ed9bb59569abc3d3d809126c2
Florian-LeCossec <florian.le-cossec@orange.fr>
2025-01-24 12:04:52 +0100

    handle animation

--- src/components/BtnList.jsx
+++ src/components/BtnList.jsx
@@ -1,13 +1,18 @@
 import useToggle from '@/hooks/toggleHook';
 import PropTypes from 'prop-types';
 import '@/styles/components/BtnList.scss';
+import { useState } from 'react';
 
 const BtnList = ({ name, list, isList}) => {
     const [isOpen, toggleOpen] = useToggle(false);
-
+    const [isVisible, setIsVisible] = useState(null);
+    
     return (
         <div className={`btn-list ${isOpen ? 'btn-list--open' : ''}`}>
-            <button onClick={toggleOpen}>
+            <button onClick={() => {
+                toggleOpen();
+                setIsVisible(!isVisible);
+            }}>
                 <span className='btn-list__name'>{name}</span>
                 <img 
                     className={isOpen ? 'btn-list__icon--open' : 'btn-list__icon--close'} 
@@ -15,16 +20,18 @@ const BtnList = ({ name, list, isList}) => {
                     alt={isOpen ? 'arrow-up' : 'arrow-down'} 
                 />
             </button>
-            {isOpen && isList && (
-                <ul className='btn-list__list'>
-                    {list.map((item, index) => (
-                        <li key={index}>{item}</li>
-                    ))}
-                </ul>
-            )}
-            {isOpen && !isList && (
-                <p className='btn-list__description'>{list}</p>
-            )}
+            <div className='btn-list__content'>
+                {isList && (
+                    <ul className='btn-list__list'>
+                        {list.map((item, index) => (
+                            <li key={index}>{item}</li>
+                        ))}
+                    </ul>
+                )}
+                {!isList && (
+                    <p className='btn-list__description'>{list}</p>
+                )}
+            </div>
         </div>
     );
 };
